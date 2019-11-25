f1c0cf497c128e7915377ec51bc22839e02cf0f2
Agne.Nevulyte-at-985443664070 <agne.nevulyte@finsaeurope.com>
2019-11-25 11:42:20 +0000

    created App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import PrivateRoute from "./ReactRouter/PrivateRoute";
+import AuthButton from "./ReactRouter/AuthButton";
+import ProtectedPage from "./containers/ProtectedPage";
+import PublicPage from "./containers/PublicPage";
+import LoginPage from "./containers/LoginPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <AuthButton />
+        <ul>
+          <li>
+            <Link to="/public">Public Page</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/public" component={PublicPage}></Route>
+          <Route path="/login" component={LoginPage}></Route>
+          <PrivateRoute path="/protected">
+            <ProtectedPage />
+          </PrivateRoute>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
